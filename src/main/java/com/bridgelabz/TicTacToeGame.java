06f0c15e7cdcd4ff5e34d3898510b05b3d50befd
unknown <madhukar.tatiraju@gmail.com>
2021-10-23 01:09:44 +0530

    UC1 added

--- src/main/java/com/bridgelabz/TicTacToeGame.java
+++ src/main/java/com/bridgelabz/TicTacToeGame.java
@@ -1,10 +1,14 @@
 package com.bridgelabz;
 
 public class TicTacToeGame {
-
     public static void main(String[] args){
         System.out.println("Welcome to TicTacToe Game");
+        createBoard();
+    }
+    public static void createBoard(){
+        char[] board = new char[10];
+        for (int i=0;i<board.length;i++){
+            board[i]=' ';
+        }
     }
 }
-
-
