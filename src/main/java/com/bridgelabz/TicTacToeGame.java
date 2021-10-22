f0a2c2a39dd04c47259dd629f5ad121b65e5e206
unknown <madhukar.tatiraju@gmail.com>
2021-10-23 01:15:26 +0530

    UC3 added

--- src/main/java/com/bridgelabz/TicTacToeGame.java
+++ src/main/java/com/bridgelabz/TicTacToeGame.java
@@ -4,15 +4,18 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class TicTacToeGame {
+
+    static char[] board = new char[10];
+
     public static void main(String[] args){
         System.out.println("Welcome to TicTacToe Game");
         createBoard();
         playerChoice();
+        showBoard();
     }
     public static void createBoard(){
-        char[] board = new char[10];
         for (int i=0;i<board.length;i++){
-            board[i]=' ';
+            board[i]= ' ';
         }
     }
     public static void playerChoice(){
@@ -26,6 +29,12 @@ public class TicTacToeGame {
         }else{
             computerPlayer = 'X';
         }
-        System.out.println("player choice is: "+player+" computer choice is: "+ computerPlayer);
+    }
+    public static void showBoard(){
+        System.out.println(" "+board[1]+" | "+board[2]+" | "+board[3]);
+        System.out.println("-----------");
+        System.out.println(" "+board[4]+" | "+board[5]+" | "+board[6]);
+        System.out.println("-----------");
+        System.out.println(" "+board[7]+" | "+board[8]+" | "+board[9]);
     }
 }
